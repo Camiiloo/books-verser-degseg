f92ddd0e36fd93614ac8c26bd5aa2cb6ae57cb28
Camilo zamora <camiiloozamora@gmail.com>
2023-11-26 16:33:15 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'BooksVerser',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
